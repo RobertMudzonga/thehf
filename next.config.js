2bdcab90c07e96eb124b1a11ef17bebf68377f61
RobetMudzonga <robertmudzonga@yahoo.com>
2022-05-27 09:18:54 +0200

    ready for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
      domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
     
   }
-}
+};
+
+
